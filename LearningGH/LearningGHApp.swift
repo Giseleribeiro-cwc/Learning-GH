56d232c19e18ff1719ae6886926e9adee0a69b91
Giseleribeiro-cwc <gisele_ribeiro@outlook.com.br>
2021-04-24 07:59:52 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Gisele Ribeiro on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
